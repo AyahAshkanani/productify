e09126f4210193330730433a7a29f4234c4f73c2
Sarah Almahmid <hpsarontm@gmail.com>
2021-08-17 18:29:17 +0300

    fix

--- components/Navigation/index.js
+++ components/Navigation/index.js
@@ -81,7 +81,7 @@ export default function MyTabs({ theme }) {
   return (
     //screen navigations
     <Stack.Navigator
-      initialRouteName="Main"
+      initialRouteName="Register"
       screenOptions={{
         headerStyle: {
           backgroundColor: "#c77dff",
